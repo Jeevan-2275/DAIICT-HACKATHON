632ae885324abc7a1eedacd517d3e2b86047e81b
Nageshjagtap <nageshjagtap063@gmail.com>
2026-01-10 00:54:32 +0530

    initialize resume analysis GET endpoint

---
+++ ai-career-coach/app/api/resume-report/latest/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { db } from "../../../../configs/db"; // adjust path if needed
+import { resumeAnalysisTable } from "../../../../configs/schema";
+import { desc } from "drizzle-orm";
+
+export async function GET() {
+
+
+
+
+
+}
